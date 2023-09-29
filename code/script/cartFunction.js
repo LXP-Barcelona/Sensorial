6d5762da89524c3632269ad7d378427821a8a8d0
ForgeOff <86209935+ForgeOfficial@users.noreply.github.com>
2023-09-30 00:02:32 +0200

    .

---
+++ code/script/cartFunction.js
@@ -0,0 +1,55 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true
+})
+
+function getCartOrCreate() {
+    let cart = localStorage.getItem("cart");
+    if (!cart)
+        localStorage.setItem("cart", JSON.stringify([]));
+    cart = localStorage.getItem("cart");
+    return JSON.parse(cart);
+}
+
+function setCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
+}
+
+function removeProduct(productId) {
+    
+}
+
+function addProduct(product, amount = 1) {
+    const cart = getCartOrCreate();
+    console.log(product)
+    const element = cart.find(c => c.id === product.id);
+    if (!element)
+        cart.push({
+            id: product.id,
+            amount: amount
+        })
+    else
+        element.amount += amount;
+    setCart(cart);
+    Toast.fire({
+        icon: 'success',
+        title: `you just added\nx${amount} ${product.name} (${(product.price*amount).toFormat()} €)`
+    })
+}
+
+
+onclick = async (event) => {
+    if (event.target.id.startsWith("addCart-")) {
+        const id = parseInt(event.target.id.replace(/addCart-/, ''));
+        const product = await findProduct(id);
+        if (product)
+            addProduct(product);
+    }
+}
