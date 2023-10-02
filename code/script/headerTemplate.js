96ebbac200998670cf3b7003277499c1553ad91c
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2023-10-02 11:42:00 +0200

    footer & header templates in JS

---
+++ code/script/headerTemplate.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    const headerElement = document.querySelector("header");
+
+    headerElement.innerHTML = 
+    `<div class="left-section">
+    <a href="#">
+        SENSORIAL
+    </a>
+</div>
+<div class="right-section">
+    <div class="products"><a href="products.html">PRODUCTS</a></div>
+    <div class="cart"><a href="cart.html">CART</a></div>
+</div>`
+}
