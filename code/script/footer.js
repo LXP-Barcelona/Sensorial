42243bc2cde6ec9b71544009e27cac3a32753568
festere <cmpnn.romain@gmail.com>
2023-10-05 11:48:38 +0200

    Update responsive

--- code/script/footer.js
+++ code/script/footer.js
@@ -1,16 +1,19 @@
 const footerElement = document.querySelector("footer");
 
 footerElement.innerHTML =
-`<div class="brand"><h1>SENSORIAL</h1></div>
-<div class="contact">
-<h2>CONTACT US</h2>
-<ul>
-    <li>+33 1 99 34 54 21</li>
-    <li>contact@sensorial.com</li>
-    <li>2 Rue des Bois, Paris</li>
-</ul>
+`
+<div class="brand"><h1>SENSORIAL</h1></div>
+    <div class="contact">
+        <h2>CONTACT US</h2>
+        <ul>
+            <li>+33 1 99 34 54 21</li>
+            <li>contact@sensorial.com</li>
+            <li>2 Rue des Bois, Paris</li>
+        </ul>
+    </div>
+    <div class="paperwork">
+        <a href="terms-of-use.html">Terms of Use</a>
+        <a href="privacy-policy.html">Privacy Policy</a>
+    </div>
 </div>
-<div class="paperwork">
-<a href="terms-of-use.html">Terms of Use</a>
-<a href="privacy-policy.html">Privacy Policy</a>
-</div>`;
+`;
