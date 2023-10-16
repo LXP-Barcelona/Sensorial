27259fa1d539db0e434854fc6f8f44505cf8d257
festere <cmpnn.romain@gmail.com>
2023-10-16 10:39:43 +0200

    Update meta description

--- code/script/lang.js
+++ code/script/lang.js
@@ -46,6 +46,7 @@ const langs = {
     es: {
         "name": "Español",
         "change-lang": "Puedes elegir otro idioma",
+
         "home-title": "Frase de presentación",
         "home-description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         "home-bestSeller": "Nuestros Mejores Vendedores",
